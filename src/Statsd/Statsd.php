2773ef2be28f3e68a0403afef17b1fe8cefa56db
Arjay Adong <arjayads@gmail.com>
2016-05-24 17:02:34 +0800

    null check

--- src/Statsd/Statsd.php
+++ src/Statsd/Statsd.php
@@ -6,12 +6,13 @@ class Statsd {
 
     private static $client;
 
-    public function __construct() {
+    public static function init() {
         $connection = new UdpSocket(env('STATSD_HOST', 'localhost'), env('STATSD_PORT', 8125));
         self::$client = new Client($connection, env('STATSD_NAMESPACE', ''));
     }
 
     public static function client(){
+        if (is_null(self::$client)) self::init();
         return self::$client;
     }
 }
