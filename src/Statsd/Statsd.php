59cfeea5a15aa2520a22f136b52ff9882443c18c
Arjay Adong <arjayads@gmail.com>
2016-05-24 16:47:55 +0800

    add client function

--- src/Statsd/Statsd.php
+++ src/Statsd/Statsd.php
@@ -4,10 +4,14 @@ namespace Statsd;
 
 class Statsd {
 
-    public static $client;
+    private static $client;
 
     public function __construct() {
         $connection = new UdpSocket(env('STATSD_HOST', 'localhost'), env('STATSD_PORT', 8125));
         self::$client = new Client($connection, env('STATSD_NAMESPACE', ''));
+    }
+
+    public static function client(){
+        return self::$client;
     }
 }
